af19b6d4563ed6c081e71bf7f2f99b7647cd3b0d
Duytv081298 <duytvgch17645@fpt.edu.vn>
2019-05-03 14:14:16 +0700

    hi

--- Dangkykh.php
+++ Dangkykh.php
@@ -59,7 +59,7 @@
 					$Password = $_POST['Password'];
 					$sql = "INSERT INTO customers(Custid, Fullname,  Address, Postalcode, City, Country, Phone, Fax, Tendangnhap, Password)  VALUES (". (int)$Custid .",'". $Fullname ."',". $Address .", '". $Postalcode."',   ". $City .", ". $Country .",    ". $Phone .",   ". $Fax .", ". $Tendangnhap .",". (int)$Password .")";
 					$sql1 ="SELECT * FROM customers WHERE Custid =".$_POST['Custid'];
-					include './ConnectorSQL.php';
+					include 'ConnectorSQL.php';
 					$row = pg_query($connection, $sql);	
 					$message = "Đã Thêm thông tin khách hàng";
 					echo "<script type='text/javascript'>alert('$message');</script>";
