8405a709d2774bdb98d6ebfc2448f2a3841e50e5
Duytv081298 <duytvgch17645@fpt.edu.vn>
2019-05-03 12:04:49 +0700

    555555

--- Cunshopdetail.php
+++ Cunshopdetail.php
@@ -44,14 +44,14 @@
 		              $categoryid = $rowcategory['categoryid'];
 		              $categoryname = $rowcategory['categoryname'];
 		          ?>
-		         <li><a href="Cunshopdetail.php?categoryid=<?= $categoryid; ?>"><?= $categoryname; ?></a></li>
+		         <li><a href="Cunshopdetail.php?categoryid= <?= $categoryid; ?> "><?= $categoryname; ?></a></li>
 		       <?php }} ?>
 			</ul>
 		</div>
 		<div>
 			<?php
 		          include 'ConnectorSQL.php';
-		            $querycategory = "SELECT * FROM category WHERE categoryid = $_GET['categoryid'] ORDER BY productid DESC LIMIT 5";
+		            $querycategory = "SELECT * FROM category WHERE categoryid = $_GET['categoryid']";
 		            $total = pg_query($connection,$querycategory);
 		            if (pg_num_rows($total) > 0) {
 		            // output data of each row
@@ -59,7 +59,7 @@
 		              $categoryid = $rowcategory['categoryid'];
 		              $categoryname = $rowcategory['categoryname'];
 		          ?>
-					<b style="font-size: 30px;"><a style="text-decoration: none; color: black;"  href="Cunshopdetail.php?categoryid=<?=$r['categoryid']?>"><?=$r['categoryname']?></a></b>
+					<b style="font-size: 30px;"><a style="text-decoration: none; color: black;"  href="Cunshopdetail.php?categoryid=<?= $categoryid; ?>"><?=$r['categoryname']?></a></b>
 
 				<?php 
 					}}
