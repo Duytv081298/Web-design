e56211f9905ab60a111d408b5766a5d98cc73c9f
Duytv081298 <duytvgch17645@fpt.edu.vn>
2019-05-03 11:31:07 +0700

    gung hoi

--- Cunshopdetail.php
+++ Cunshopdetail.php
@@ -17,20 +17,20 @@
 				<li><a href="https://designweb.herokuapp.com/admin.php">Admin</a></li>
 			</ul>
 			</div>
-		 	
-		<div class="Home">
-			<a href="https://designweb.herokuapp.com/Cunshop.php">Cun Shop</a>
-		</div>
-		<div class="Search">
-			<div class="Search1">
-				<form class="example" action="Search.php" method="get">
-  				<input type="text" placeholder="Search.." name="search">
-  				<button type="submit"><i class="fa fa-search"></i></button>
-				</form>
-			</div>
-		</div>
+		 	<div class="banner">
+		 		<div class="Home">
+					<a href="https://designweb.herokuapp.com/Cunshop.php">Cun Shop</a>
+				</div>
+				<div class="Search">
+					<div class="Search1">
+						<form class="example" action="Search.php" method="get">
+		  				<input type="text" placeholder="Search.." name="search">
+		  				<button type="submit"><i class="fa fa-search"></i></button>
+						</form>
+					</div>
+				</div>
+		 	</div>
 	</div>
-
 	<div class="main">
 		<div class="navb">
 			<ul>
@@ -98,26 +98,23 @@
 			}}
 			?>
 		</div>
-	</div>
+	</div>		
 	<div class="footer">
 		<table  cellspacing="0" cellpadding="10" width= 100% align="center" >
 			<tr >
-			<th style="font-size: 17px" >CUNSOP_KÊNH MUA SẮM & DỊCH VỤ TRỰC TUYẾN HÀNG ĐẦU VIỆT NAM!</th>
-			<th  rowspan="2" > CÔNG TY TNHH Duy Thắng <br>
+			<th style="font-size: 17px; color:#FFFFFF"  >CUNSOP_KÊNH MUA SẮM & DỊCH VỤ TRỰC TUYẾN HÀNG ĐẦU VIỆT NAM!</th>
+			<th  rowspan="2" style=" color:#FFFFFF" > CÔNG TY TNHH Duy Thắng <br>
  													Giấy CNĐKDN: 289037490 – Ngày cấp: 06/5/2005, được sửa đổi lần thứ 17 ngày 24/7/2017. <br>
  													Cơ quan cấp: Phòng Đăng ký kinh doanh – Sở kế hoạch và Đầu tư hà Nội. <br>
  													Địa chỉ đăng ký kinh doanh: Tầng 71, Tòa Nhà Keangnam, E6, Phạm Hùng, Phường Mễ Trì, Quận Nam Từ Liêm, Hà Nội, Việt Nam <br>  <br><br><br>     @Cunshop 2018
  			</th>
 			</tr>
 			<tr >
-				<td ><div align="center" style="padding-top:0%, width= 20px" >
-					
-				</style>Mua hàng trực tuyến (mua hàng online) mang lại sự tiện lợi, lựa chọn đa dạng hơn và các dịch vụ tốt hơn cho người tiêu dùng, thế nhưng người tiêu dùng Việt Nam vẫn chưa tận hưởng được những tiện ích đó. Chính vì vậy Cunshop Việt Nam được triển khai với mong muốn trở thành trung tâm mua sắm trực tuyến số 1 tại Việt Nam, nơi bạn có thể chọn lựa mọi thứ để chăm sóc thú cưng của mình, từ thức ăn cho chó, thức ăn cho mèo, chuồng chó, chuồng cho mèo, và các cả quàn áo cho chúng, các dịch vụ chăm sóc dành riêng cho thú cưng... Chúng tôi có tất cả!</div></td>
-				
-				
+				<td ><div align="center" style="padding-top:0%, width= 20px; color:#FFFFFF" >	
+				</style>Mua hàng trực tuyến (mua hàng online) mang lại sự tiện lợi, lựa chọn đa dạng hơn và các dịch vụ tốt hơn cho người tiêu dùng, thế nhưng người tiêu dùng Việt Nam vẫn chưa tận hưởng được những tiện ích đó. Chính vì vậy Cunshop Việt Nam được triển khai với mong muốn trở thành trung tâm mua sắm trực tuyến số 1 tại Việt Nam, nơi bạn có thể chọn lựa mọi thứ để chăm sóc thú cưng của mình, từ thức ăn cho chó, thức ăn cho mèo, chuồng chó, chuồng cho mèo, và các cả quàn áo cho chúng, các dịch vụ chăm sóc dành riêng cho thú cưng... Chúng tôi có tất cả!</div></td>	
 			</tr>
-			
 		</table>
 	</div>
 </body>
 </html>
+
