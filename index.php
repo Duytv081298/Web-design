bfc335898f3e8c0f38527e19133a45edc42a5c87
Duytv081298 <duytvgch17645@fpt.edu.vn>
2019-05-04 17:02:26 +0700

    2142353456

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 
 		     include 'ConnectorSQL.php';
 
-		    $queryfirst = "SELECT * from product order by product desc fetch first 10 rows only";
+		    $queryfirst = "SELECT * from product order by product desc fetch first 9 rows only";
 		    $resultfirst = pg_query($connection,$queryfirst);
 		    if (pg_num_rows($resultfirst) > 0) {
 		      // output data of each row
